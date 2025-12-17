3324d6814ca208df1f7c586f24ae840d1a77e199
Park Tae Wook <parrot2155@gmail.com>
2025-12-17 16:38:54 +0900

    [FEATURE] 초기 세팅

---
+++ backend/src/main/java/com/MyInsta/My_Instagram/MyInstagramApplication.java
@@ -0,0 +1,13 @@
+package com.MyInsta.My_Instagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyInstagramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyInstagramApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/MyInsta/My_Instagram/MyInstagramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MyInsta.My_Instagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyInstagramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
